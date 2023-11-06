a69955a8b98fd82a67b5d2a1a0f2aafe12143f24
Vasjan Çupri <vasjancupri04@gmail.com>
2023-11-06 21:18:04 +0100

    Made the UI a bit better and fixed a logical error

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
 const Player = (username) => {
-  const markers = ["X", "O"]
+  const markers = ['X', 'O']
   const marker = markers[Math.floor(Math.random() * 2)]
   return { username, marker }
 }
 
-const boxes = document.querySelectorAll(".box")
+const boxes = document.querySelectorAll('.box')
 
-const player1Inp = document.getElementById("player1Name")
-const player2Inp = document.getElementById("player2Name")
+const player1Inp = document.getElementById('player1Name')
+const player2Inp = document.getElementById('player2Name')
 
-const questionaire = document.getElementById("questionaire")
+const questionaire = document.getElementById('questionaire')
 
-const continueGame = document.getElementById("continue-game")
+const continueGame = document.getElementById('continue-game')
 
-const newGame = document.querySelector("#new-game")
+const newGame = document.querySelector('#new-game')
 
 let player1
 let player2
 
 let marker
 
-continueGame.addEventListener("click", () => {
-  player1 = player1Inp.value ? Player(player1Inp.value) : Player("Player 1")
-  player2 = player2Inp.value ? Player(player2Inp.value) : Player("Player 2")
+continueGame.addEventListener('click', () => {
+  player1 = player1Inp.value ? Player(player1Inp.value) : Player('Player 1')
+  player2 = player2Inp.value ? Player(player2Inp.value) : Player('Player 2')
+  if (player1.marker == player2.marker) {
+    player2.marker = player1.marker == 'X' ? 'O' : 'X'
+  }
 
-  console.log(player1)
-  console.log(player2)
-  questionaire.style.display = "none"
-  marker = ["X", "O"][Math.floor(Math.random() * 2)]
-  document.getElementById("player-turn").textContent =
+  questionaire.style.display = 'none'
+  marker = ['X', 'O'][Math.floor(Math.random() * 2)]
+  document.getElementById('player-turn').textContent =
     player1.marker == marker ? player1.username : player2.username
-  document.querySelector("main").style.display = "block"
-  console.log(marker)
-  document.getElementById("player1-username").textContent = player1.username
-  document.getElementById("player2-username").textContent = player2.username
-  document.getElementById("player1-marker").textContent = player1.marker
-  document.getElementById("player2-marker").textContent = player2.marker
+  document.querySelector('main').style.display = 'block'
+  document.getElementById('player1-username').textContent = player1.username
+  document.getElementById('player2-username').textContent = player2.username
+  document.getElementById('player1-marker').textContent = player1.marker
+  document.getElementById('player2-marker').textContent = player2.marker
 })
 
 const Game = {
   gameBoxes: [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
   ],
   boxreseter: () => {
-    boxes.forEach((box) => (box.textContent = ""))
+    boxes.forEach((box) => (box.textContent = ''))
     Game.gameBoxes = [
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""],
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
     ]
   },
   checkWin: (playerMarker) => {
@@ -84,7 +84,7 @@ const Game = {
   },
   isDraw: () => {
     for (const row of Game.gameBoxes) {
-      if (row.includes("")) {
+      if (row.includes('')) {
         return false
       }
     }
@@ -94,41 +94,40 @@ const Game = {
 
 function boxClickHandler() {
   const box = this
-  if (box.textContent == "") {
+  if (box.textContent == '') {
     box.textContent = marker
-    Game.gameBoxes[Number(box.getAttribute("row"))][
-      Number(box.getAttribute("column"))
+    Game.gameBoxes[Number(box.getAttribute('row'))][
+      Number(box.getAttribute('column'))
     ] = marker
     if (Game.checkWin(marker)) {
-      document.querySelector("h2").style.display = "none"
-      document.querySelector("#winner").style.display = "block"
-      document.querySelector("#winner").textContent = `${
+      document.querySelector('h2').style.display = 'none'
+      document.querySelector('#winner').style.display = 'block'
+      document.querySelector('#winner').textContent = `${
         player1.marker == marker ? player1.username : player2.username
       } is the winner`
-      newGame.style.display = "block"
-      boxes.forEach((box) => box.removeEventListener("click", boxClickHandler))
+      newGame.style.display = 'block'
+      boxes.forEach((box) => box.removeEventListener('click', boxClickHandler))
     } else if (Game.isDraw()) {
-      document.querySelector("h2").style.display = "none"
-      document.querySelector("#winner").style.display = "block"
-      document.querySelector("#winner").textContent = "It's a draw!"
-      newGame.style.display = "block"
-      boxes.forEach((box) => box.removeEventListener("click", boxClickHandler))
+      document.querySelector('h2').style.display = 'none'
+      document.querySelector('#winner').style.display = 'block'
+      document.querySelector('#winner').textContent = "It's a draw!"
+      newGame.style.display = 'block'
+      boxes.forEach((box) => box.removeEventListener('click', boxClickHandler))
     } else {
-      marker = marker == "X" ? "O" : "X"
-      console.log(Game.gameBoxes)
-      document.getElementById("player-turn").textContent =
+      marker = marker == 'X' ? 'O' : 'X'
+      document.getElementById('player-turn').textContent =
         player1.marker == marker ? player1.username : player2.username
     }
   }
 }
 
-boxes.forEach((box) => box.addEventListener("click", boxClickHandler))
+boxes.forEach((box) => box.addEventListener('click', boxClickHandler))
 
-newGame.addEventListener("click", () => {
+newGame.addEventListener('click', () => {
   Game.boxreseter()
-  document.querySelector("h2").style.display = "block"
-  document.querySelector("#winner").textContent = ``
-  newGame.style.display = "none"
+  document.querySelector('h2').style.display = 'block'
+  document.querySelector('#winner').textContent = ``
+  newGame.style.display = 'none'
 
-  boxes.forEach((box) => box.addEventListener("click", boxClickHandler))
+  boxes.forEach((box) => box.addEventListener('click', boxClickHandler))
 })
